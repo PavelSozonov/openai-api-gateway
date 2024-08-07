49225468b199ef909434eefb71fd006040d47e25
Pavel Sozonov <pavel.sozonov@gmail.com>
2024-08-07 04:28:43 +0300

    fix(gateway): Comment out rate limiting directives in nginx.conf
    The rate limiting directives in the nginx.conf file were causing issues. They have been commented out to resolve the problem.

--- image/init.sh
+++ image/init.sh
@@ -3,11 +3,13 @@
 # Substitute environment variables
 envsubst '${GATEWAY_API_KEY},${OPENAI_API_KEY},${OPENAI_API_HOST},${DOMAIN},${RATE_LIMIT},${CLIENT_MAX_BODY_SIZE},${CORS_ORIGIN},${CORS_METHODS},${CORS_HEADERS},${CORS_CREDENTIALS},${CORS_MAX_AGE},${CONTENT_LENGTH},${CONTENT_TYPE}' < /etc/nginx/templates/gateway-nginx.conf.template > /etc/nginx/conf.d/default.conf
 
+set -xe
+
 # Start nginx in the background
 nginx &
 
 # Wait for Nginx to be ready
-sleep 5
+sleep 10
 
 # Request a certificate from Let's Encrypt
 certbot --nginx -d ${DOMAIN} --non-interactive --agree-tos --email ${EMAIL}
